0e0f89c677a7ac1f41d19bd8fac33694cdc62591
Nishat7155 <57759578+Nishat7155@users.noreply.github.com>
2023-04-27 21:05:23 +0200

    Update isTwoPair.test.js

--- test/isTwoPair.test.js
+++ test/isTwoPair.test.js
@@ -1,21 +1,26 @@
-const CompareHands = require('../CompareHands');
 const Hand = require('../Hand');
+const CompareHands = require('../CompareHands');
 const suits = '♥♦♣♠';
 
-test('check that isTwoPair returns truthy if two pair', () => {
-  let hand = new Hand('♦2', '♥2', '♠3', '♥3', '♥8');
-  expect(CompareHands.isTwoPair(hand)).toBeTruthy();
+test('Check that isTwoPair returns truthy is two pair', () => {
+	let hand = new Hand('♥J','♠J','♣3','♠3','♥2');
+	expect(CompareHands.isTwoPair(hand)).toBeTruthy();
+})
+
+test('Check that isTwoPair returns falsy is not two pair', () => {
+	let hand = new Hand('♥7','♦3','♣2','♠6','5♠');
+	expect(CompareHands.isTwoPair(hand)).toBeFalsy();
 });
 
-test('check that isTwoPair returns falsy if not two pair', () => {
-  let hand = new Hand('♥3', '♣3', '♠7', '♣4', '♣8');
-  expect(CompareHands.isTwoPair(hand)).toBeFalsy();
+test('Check that falsy isTwoPair returns as 0', () => {
+	let hand = new Hand('♥7','♦3','♣2','♠6','5♠');
+	expect(CompareHands.isTwoPair(hand)).toBe(0);
 });
 
-test('check that isTwoPair returns a higher score for a stronger hand (if two hands both with two pair)', () => {
-  let hand1 = new Hand('♣2', '♥2', '♣5', '♥5', '♦6');
-  let hand2 = new Hand('♦2', '♦2', '♦5', '♠5', '♦7');
+test('test that isTwoPair returns a higher score for a stronger hand (if two hands but with flush)', () => {
+  let hand1 = new Hand('♥J','♠J','♣3','♠3','♥2');
+  let hand2 = new Hand('♥7','♦3','♣2','♠6','5♠');
   let hand1Score = CompareHands.isTwoPair(hand1);
   let hand2Score = CompareHands.isTwoPair(hand2);
-  expect(hand2Score).toBeGreaterThan(hand1Score);
+  expect(hand1Score).toBeGreaterThan(hand2Score);
 });
